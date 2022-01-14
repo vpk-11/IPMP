2173fe4da688e202a6ef6d902e0547e0f49b8f3f
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2022-01-14 19:40:13 +0530

    Update BM_01.cpp

--- Week 2/Bit Manupilation/BM_01.cpp
+++ Week 2/Bit Manupilation/BM_01.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
+// Power of 2
+#include <iostream>
 using namespace std;
 
-int main(){
+int isPowerOfTwo(int n)
+{
+    return n && !(n & n-1);
+}
+
+int main()
+{
+    int num;
+    cin>>num;
+    if(isPowerOfTwo(num)){
+        cout<<num<<" is a power of 2"<<endl;
+    } else{
+        cout<<num<<" isn't a power of 2"<<endl;
+    }
+    
     return 0;
 }
